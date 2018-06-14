0663ce878386572f4d706f310f8957e1164fa775
Ivan Slepovichev <slepovichevii@vnipigaz.gazprom.ru>
2018-06-14 17:50:09 +0400

    Начата переделка способа вычисления схожести термов

--- NLDB/Confidence.cs
+++ NLDB/Confidence.cs
@@ -6,18 +6,11 @@ using System.Threading.Tasks;
 
 namespace NLDB
 {
+    using WordVector = Dictionary<Word, double>;
+    public enum CompareFunctionType { Cosine, Injection, OrderedInjection };
+
     public class Confidence
     {
-        private readonly Lexicon lexicon;
-        //Массив функций для поэлементного вычисления "похожести" векторов слов. Индекс в массиве - ранг сравниваемых слов.
-        //Функция применяется к двум скалярным элементам веторов, в соответствующих позициях
-        private readonly Func<double, double, double>[] PointwiseOperations = new Func<double, double, double>[4]
-        {
-            Confidence.DotOverlap,
-            Confidence.DotOverlap,
-            Confidence.DotNorm2,
-            Confidence.DotExists
-        };
 
         public Confidence(Lexicon l)
         {
@@ -36,74 +29,109 @@ namespace NLDB
             }
         }
 
-        public void EvaluateNearest(Term term)
+        public void EvaluateNearest(Term a)
         {
-            Dictionary<Word, double> confidences = new Dictionary<Word, double>();
-            //Получаем список подслов, входящих в терм term.Childs
-            var subwords = term.Childs.Select(t => this.lexicon.Child[t.Id]).ToList();
-            //Вычисляем оценки всех возможных вариантов соответствия терму term
-            for (int i = 0; i < subwords.Count; i++)
+            var lex = this.lexicon;
+            var sublex = this.lexicon.Child;
+            if (a.Rank == 0)
             {
-                double k = term.Childs[i].Confidence;
-                subwords[i].Parents.ForEach(link =>
-                {
-                    Word p = this.lexicon[link.Id];
-                    if (!confidences.ContainsKey(p)) confidences.Add(p, 0);
-                    confidences[p] += this.PointwiseOperations[term.Rank](i, link.pos) * k;
-                });
+                this.EvaluateNearestToAtom(a);
+                return;
             }
-            //Вычисляем максимум confidence как cos(терм, слово) по всем словам Лексикона
-            //cos(a,b) = (a*b)/(|a|*|b|)
-            //В нашем случае m=max(длина_терма, длина_слова),
-            //следовательно |a|*|b|=Sqrt(<a,a>)*Sqrt(<b,b>)=Sqrt(<m,m>)*Sqrt(<m,m>)=m           
-            confidences.Aggregate(term, (c, n) =>
-            {
-                double value = n.Value / Math.Max(n.Key.Childs.Length, term.Childs.Count);
-                if (value > c.Confidence)
+            //Выделение претендентов на роль ближайшего
+            var parents = a.Childs.SelectMany(t => sublex[t.Id].Parents.
+                Select(link => lex[link.Id])).
+                Distinct().
+                Select(p => new Term(p, sublex));
+            //Поиск ближайшего
+            var nearest = parents.Aggregate<Term, Term>(null,
+                (c, n) =>
                 {
-                    c.Id = n.Key.Id;
-                    c.Confidence = value;
+                    if (c == null) return n;
+                    if (c.Confidence < this.Compare(a, n)) return n;
                     return c;
-                }
-                else
-                    return c;
-            });
+                });
+            a = nearest;
+            //Dictionary<Word, double> confidences = new Dictionary<Word, double>();
+            ////Получаем список подслов, входящих в терм term.Childs
+            //var subwords = term.Childs.Select(t => this.lexicon.Child[t.Id]).ToList();
+            ////Вычисляем оценки всех возможных вариантов соответствия терму term
+            //for (int i = 0; i < subwords.Count; i++)
+            //{
+            //    double k = term.Childs[i].Confidence;
+            //    subwords[i].Parents.ForEach(link =>
+            //    {
+            //        Word p = this.lexicon[link.Id];
+            //        if (!confidences.ContainsKey(p)) confidences.Add(p, 0);
+            //        confidences[p] += this.PointwiseOperations[term.Rank](i, link.pos) * k;
+            //    });
+            //}
+            ////Вычисляем максимум confidence как cos(терм, слово) по всем словам Лексикона
+            ////cos(a,b) = (a*b)/(|a|*|b|)
+            ////В нашем случае m=max(длина_терма, длина_слова),
+            ////следовательно |a|*|b|=Sqrt(<a,a>)*Sqrt(<b,b>)=Sqrt(<m,m>)*Sqrt(<m,m>)=m           
+            //confidences.Aggregate(term, (c, n) =>
+            //{
+            //    double value = n.Value / Math.Max(n.Key.Childs.Length, term.Childs.Count);
+            //    if (value > c.Confidence)
+            //    {
+            //        c.Id = n.Key.Id;
+            //        c.Confidence = value;
+            //        return c;
+            //    }
+            //    else
+            //        return c;
+            //});
+        }
 
-            //for (int i = 0; i < term.Childs.Count; i++)
-            //{
-            //    var subterm = term.Childs[i];
-            //    if (subterm.Id == -1) throw new ArgumentException("Невозможно определить слово для терма " + subterm.Text);
-            //    Word subword = this.lexicon.Child[subterm.Id];
-            //    var links = subword.Parents;
-            //    foreach (var link in links)
-            //    {
-            //        Word word = this.lexicon[link.Id];
-            //        //С учетом порядка букв в слове
-            //        if (!confidences.ContainsKey(word)) confidences[word] = 0;
-            //        switch (term.Rank)
-            //        {
-            //            case 1: confidences[word] += DotMul(i, link.pos) * subterm.Confidence; break;
-            //            case 2: confidences[word] += ConstOne(i, link.pos) * subterm.Confidence; break;
-            //            case 3: confidences[word] += ConstOne(i, link.pos) * subterm.Confidence; break;
-            //            default: throw new ArgumentOutOfRangeException("Для слов ранга >3 вычисление расстояния не реализовано");
-            //        }
-            //    }
-            //}
-            //double t_size = term.Childs.Count;
-            //foreach (var d in confidences)
-            //{
-            //    double w_size = d.Key.Childs.Length;
-            //    //cos(a,b) = (a*b)/(|a|*|b|)
-            //    //В нашем случае m=max(длина_терма, длина_слова),
-            //    //следовательно |a|*|b|=Sqrt(<a,a>)*Sqrt(<b,b>)=Sqrt(<m,m>)*Sqrt(<m,m>)=m,                 
-            //    double denominator = Math.Max(w_size, t_size);
-            //    double value = d.Value / denominator;
-            //    if (value > term.Confidence)
-            //    {
-            //        term.Confidence = value;
-            //        term.Id = d.Key.Id;
-            //    }
-            //}
+        public double Compare(Term a, Term b)
+        {
+            if (a.Rank != b.Rank) throw new ArgumentException("Попытка сравнить термы разных рангов");
+            return 
+        }
+
+        //Возвращает 1, если вектор a полностью входит в b
+        private static double Inclusive(Term a, Term b)
+        {
+            int count = a.Childs.Aggregate(0, (c, n) => b.Contains(n) ? c + 1 : c);
+            return (count == a.Count) ? 1 : 0;
+        }
+
+        private static double SoftInclusive(Term a, Term b)
+        {
+            double count = a.Childs.Aggregate<Term, double>(0, (c, n) => b.Contains(n) ? c + n.Confidence : c);
+            return count / a.Count;
+        }
+
+        private static double Cosine(Term a, Term b)
+        {
+            int n = Math.Min(a.Count, b.Count);
+            double s = 0;
+            for (int i = 0; i < n; n++)
+                s += (a.Childs[i].Id == b.Childs[i].Id) ? a.Childs[i].Confidence : 0;
+            double denominator = a.Count * b.Count;
+            return s / denominator;
+        }
+
+        private static double SoftCosine(Term a, Term b)
+        {
+            double s = 0;
+            double da = 0, db = 0;
+            for (int i = 0; i < a.Count; i++)
+                for (int j = 0; j < b.Count; j++)
+                    s += this.Compare(a.Childs[i], b.Childs[j]);
+            for (int i = 0; i < a.Count; i++)
+                for (int j = 0; j < a.Count; j++)
+                    da += this.Compare(a.Childs[i], a.Childs[j]);
+            for (int i = 0; i < b.Count; i++)
+                for (int j = 0; j < b.Count; j++)
+                    db += this.Compare(b.Childs[i], b.Childs[j]);
+            return s / (Math.Sqrt(da) * Math.Sqrt(db));
+        }
+
+        private static double Equality(Term a, Term b)
+        {
+            return a.Id == b.Id ? 1 : 0;
         }
 
         private void EvaluateNearestToAtom(Term term)
@@ -113,25 +141,18 @@ namespace NLDB
             return;
         }
 
-        private static double DotOverlap(double a, double b)
+        //----------------------------------------------------------------------------------------------------------------
+        //Частные свойства и поля
+        private readonly Lexicon lexicon;
+        //Массив функций для поэлементного вычисления "похожести" векторов слов. Индекс в массиве - ранг сравниваемых слов.
+        //Функция применяется к двум скалярным элементам веторов, в соответствующих позициях
+        private readonly Func<Term, Term, double>[] Operations = new Func<Term, Term, double>[4]
         {
-            return (a == b) ? 1 : 0;
-        }
-
-        private static double DotExists(double a, double b)
-        {
-            return 1;
-        }
-
-        private static double DotNorm1(double a, double b)
-        {
-            return Math.Abs(a - b);
-        }
-
-        private static double DotNorm2(double a, double b)
-        {
-            return (a - b) * (a - b);
-        }
+            Confidence.Equality,
+            Confidence.Cosine,
+            Confidence.SoftCosine,
+            Confidence.ConstOne
+        };
 
     }
 }
--- NLDB/Term.cs
+++ NLDB/Term.cs
@@ -13,7 +13,8 @@ namespace NLDB
         public int Id;
         public int Rank;
         public double Confidence;
-        public List<Term> Childs = new List<Term>();
+        public readonly List<Term> Childs = new List<Term>();
+        public readonly Dictionary<Term, int> ChildsBag = new Dictionary<Term, int>();
 
         public Term(string _t, List<Term> _childs)
         {
@@ -22,11 +23,69 @@ namespace NLDB
             this.Confidence = 0;
             this.Childs = _childs;
             if (this.Childs.Count > 0)
+            {
+                this.ChildsBag = this.Childs.
+                    Distinct().
+                    Select((t, i) => new KeyValuePair<Term, int>(t, i)).
+                    ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
                 this.Rank = this.Childs[0].Rank + 1;
+            }
             else
                 this.Rank = 0;
         }
 
+        public Term(Word w, Lexicon lex)
+        {
+            this.Text = ""; //для скорости можно было бы lex.ToText(w)
+            this.Id = w.Id;
+            this.Confidence = 1;
+            if (w.Childs.Length > 0)
+            {
+                this.Childs = w.Childs.Select(c => new Term(lex.Child[c], lex.Child)).ToList();
+                this.ChildsBag = this.Childs.
+                    Distinct().
+                    Select((t, i) => new KeyValuePair<Term, int>(t, i)).
+                    ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                this.Rank = this.Childs[0].Rank + 1;
+            }
+            else
+                this.Rank = 0;
+        }
+
+        public bool Contains(Term c)
+        {
+            return this.ChildsBag.ContainsKey(c);
+        }
+
+        public int Count { get { return this.Childs.Count; } }
+
         public override string ToString() => this.Text;
+
+
+        public override bool Equals(object obj)
+        {
+            Term w = (Term)obj;
+            //Если указан id, то сравниваем по id (состав может отличаться). Такой способ нужен для поиска с неизвестным составом
+            if (this.Id == w.Id) return true;
+            //Если длины слов не равны то слова не равны
+            if (w.Childs.Count != this.Childs.Count) return false;
+            if (w.Childs.Count == 0) return w.Id == this.Id;
+            for (int i = 0; i < this.Childs.Count; i++)
+                if (w.Childs[i] != this.Childs[i]) return false;
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            if (this.Childs.Count == 0)
+                return this.Id;
+            int hash = 0;
+            for (int i = 0; i < this.Childs.Count; i++)
+            {
+                hash += this.Childs[i].Id + 1013904223;
+                hash *= 1664525;
+            }
+            return hash;
+        }
     }
 }
