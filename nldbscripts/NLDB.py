c8ad669e248069034cdb1417d29329391cc45395
Иван Слеповичев <gurgutan@yandex.ru>
2019-02-07 16:26:02 +0400

    Добавлен Python проект для вычислений

---
+++ nldbscripts/NLDB.py
@@ -0,0 +1,15 @@
+import sqlite3 as sqlite
+import numpy as np
+import scipy as sc
+
+class Calculations(object):
+    """description of class"""
+    
+    def __init__(self, conn):
+        self.connection = conn
+
+
+
+
+
+
