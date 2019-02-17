398bd39f7348ef931815683b2d40e037dab59398
Иван Слеповичев <gurgutan@yandex.ru>
2019-02-17 14:14:11 +0400

    Добавлен tokenizer (надо доделать возврат БД)

--- nldbscripts/splitter.py
+++ nldbscripts/splitter.py
@@ -4,11 +4,14 @@ import re
 class Splitter(object):
     """Класс реализующий методы работы с исходным текстом"""
 
-    def __init__(self, splitters):
-        self.splitters = splitters
-        self._repls = [
-            [r"[^а-яА-ЯёЁ\d\s\n\!\.\,\;\:\*\+\-\&\\\/\%\$\^\[\]\{\}\=\<\>]", ""]]
+    splitters = [r'[^а-яёА-ЯЁ\d\{\}\-]+', r'[\n\r\?\!\:\;]+', r'\[\[\d+\]\]']
+
+    def __init__(self, splitters=None):
+        if splitters != None:
+            self.splitters = splitters
+        self._remove_pattern = re.compile(r'[^а-яА-ЯёЁ\d\s\n\!\.\,\;\:\*\+\-\&\\\/\%\$\^\[\]\{\}\=\<\>]')
         self.lower = True
+        self.patterns = [re.compile(x) for x in splitters]
 
     def length(self):
         return len(self.splitters)
@@ -25,16 +28,15 @@ class Splitter(object):
 
     def _split(self, text, rank):
         if(rank < 0 or rank >= self.length()):
-            return ""
+            return None
+        terms = self.patterns[rank].split(text)
         if rank == 0:
-            return re.split(self.splitters[rank], text)
-        result = [self._split(t, rank-1)
-                  for t in re.split(self.splitters[rank], text)]
+            result = [s for s in terms if len(s)>0]
+        else:
+            terms
+            result = [self._split(t, rank-1) for t in terms if len(t)>0]
         return result
 
     def format(self, text):
-        if self.lower:
-            result = text.lower()
-        for repl in self._repls:
-            result = re.sub(repl[0], repl[1], result)
+        result = self._remove_pattern.sub('', text.lower())
         return result
--- nldbscripts/tokenizer.py
+++ nldbscripts/tokenizer.py
@@ -1,19 +1,48 @@
-import splitter
+import sqlite3 as sqlite
+import numpy as np
+
 
 
 class Tokenizer(object):
+    """Класс с методами для преобразования дерева строк в дерево токенов"""
+
     def __init__(self):
-        self.id = 0
+        self.id = 65535
+        self.db = sqlite.connect(':memory:')
+        self.db.execute(
+            'create table words(id INTEGER PRIMARY KEY, childs BLOB not null)')
+        self.db.execute('create unique index childs on words(childs)')
+        self.db.commit()
+
+    def __del__(self):
+        self.db.close()
+
+    def get_token(self, childs):
+        a = np.array(childs, dtype=np.int32)
+        cur = self.db.cursor()
+        cur.execute(
+            'select id from words where childs=?', (a.tobytes(),))
+        result = cur.fetchone()
+        if result == None:
+            self.id += 1
+            self.db.execute('insert into words(id, childs) values(?,?)',
+                            (self.id, a.tobytes()))
+            self.db.commit()
+            token=self.id
+        else:
+            token=result[0]
+        return token
 
     def tokenize(self, text_tree, rank):
-        return
-        {
-            ord(t): None if rank == 0 else
-            self._get_id(t): {self.tokenize(t, rank-1)}
-            !!!
-        }
-
-    def _get_id(self, ):
-        pass
-
-    
+        if rank == 0:
+            return [ord(c) for c in text_tree]
+        tokens=[]
+        for t in text_tree:
+            childs=self.tokenize(t, rank-1)
+            if len(childs) == 0:
+                continue
+            id=self.get_token(childs)
+            if id == None:
+                id=self.get_token(childs)
+            tokens.append(id)
+        return tokens
